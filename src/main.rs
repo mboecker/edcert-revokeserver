f769d23e150027cb04f370f3529b6f33dc743e7e
xenor <xenorlawl@gmail.com>
2016-03-10 15:28:06 +0100

    Moved from a thread for each request to a threadpool-based model

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+extern crate threadpool;
+extern crate num_cpus;
+
 use std::io::Write;
 use std::io::Read;
 use std::net::TcpListener;
-use std::thread;
+
+use threadpool::ThreadPool;
 
 fn main() {
+
+    // create new threadpool
+    let pool = ThreadPool::new(num_cpus::get());
+    println!("Creating {} threads...", pool.max_count());
 
     // create the listener
     let listener = TcpListener::bind("0.0.0.0:41001").expect("Failed to create TCP Listener");
@@ -11,7 +19,9 @@ fn main() {
 
     // accept connections
     for stream in listener.incoming() {
-        thread::spawn(|| {
+
+        // start a process in the thread
+        pool.execute(|| {
             let mut stream = stream.expect("Failed to accept Connection");
             let mut buf = [0; 32];
 
