e094db3a3a18a4187017a2f23d5e28c6951baa86
xenor <xenorlawl@gmail.com>
2016-03-11 16:49:11 +0100

    Added a 10 second timeout to the read operation. after 10 seconds, the socket is closed.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io::Read;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     // create the listener
@@ -12,17 +13,30 @@ fn main() {
     // accept connections
     for stream in listener.incoming() {
         let stream = stream.expect("Failed to accept connection");
+
+        // spawn thread for each connection
         thread::spawn(|| handle_stream(stream));
     }
 }
 
 fn handle_stream(mut stream: TcpStream) {
+
+    // wait 10 seconds max
+    stream.set_read_timeout(Some(Duration::from_secs(10))).expect("Failed to set timeout");
+
+    // allocate buffer
     let mut buf = [0; 32];
 
     // read public key
-    stream.read_exact(&mut buf).expect("Failed to read public key");
-    println!("{:?}", buf);
+    match stream.read_exact(&mut buf) {
+        Ok(_) => {
+            println!("{:?}", buf);
 
-    // write response
-    stream.write(b"ok").expect("Failed to write response");
+            // write response
+            stream.write(b"ok").expect("Failed to write response");
+        },
+        _ => {
+            stream.write(b"timeout").expect("Failed to write response");
+        }
+    }
 }
